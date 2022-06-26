8ac026441d08ca1f3ff2c52e5a6bdfd54c2acbfd
Wanessa Koserski <wkoserski@gmail.com>
2022-06-26 12:43:25 -0300

    foreach + lambda

--- java-atividades/ApredendoLambda/src/utilFunction/Foreach.java
+++ java-atividades/ApredendoLambda/src/utilFunction/Foreach.java
@@ -9,6 +9,29 @@ public class Foreach {
 		
 		List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia");
 
+		System.out.println("forma tradicional");
+		for (String nome: aprovados)
+		{
+			System.out.println(nome);
+		}
+		
+		System.out.println("\nLambda 01");
+		aprovados.forEach((nome) ->  System.out.println(nome + "!") );
+		
+		System.out.println("\nMethod Reference 01");
+		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLambda 02");
+		aprovados.forEach(nome ->  imprimir(nome));
+		
+		System.out.println("\nMethod Reference 02");
+		aprovados.forEach(Foreach::imprimir);
+		
 	}
+	
+	static void imprimir(String nome) {
+		System.out.println("Oi, meu nome é " + nome);
+	}
+	
 
 }
