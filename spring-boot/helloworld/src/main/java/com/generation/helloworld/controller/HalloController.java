7e5b94967a9d9754ff4541a50988cfc037914c37
Wanessa Koserski <wkoserski@gmail.com>
2022-06-28 11:51:39 -0300

    new endpoints

--- spring-boot/helloworld/src/main/java/com/generation/helloworld/controller/HalloController.java
+++ spring-boot/helloworld/src/main/java/com/generation/helloworld/controller/HalloController.java
@@ -1,8 +1,9 @@
 package com.generation.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,6 +12,32 @@ public class HalloController {
 	@GetMapping
 	public String hello() {
 		return "hello world";
+	}
+	
+	@GetMapping("/{name}")
+	public String objetivos(@PathVariable String name) {
+		String retorno = "Meu nome é " + name
+				+ "\nMeus objetivos para essa semana é "
+				+ "estudar Spring Boot e Java";
+		return retorno;
+	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		String ListaBSM = 
+				"\n** Mentalidade **"
+				+ "\n - Orientação ao Futuro"
+				+ "\n - Responsabilidade Pessoal"
+				+ "\n - Mentalidade de Crescimento"
+				+ "\n - Persistência"
+				+ "\n"
+				+ "\n** Competência **"
+				+ "\n - Comunicação"
+				+ "\n - Proatividade"
+				+ "\n - Orientação ao Detalhe"
+				+ "\n - Trabalho em Equipe";
+		
+		return ListaBSM;
 	}
 	
 }
